50b928ccbec365ead0e63caa92ef2c861af65e7f
Bruno <bruno.l.motta@unesp.br>
2026-03-19 19:26:29 -0300

    19-03-2026, projs-php

---
+++ atvindividual.php
@@ -0,0 +1,7 @@
+<?php
+$cidade = "Bauru";
+$dia = 1;
+$mes = "março";
+$ano = 2024;
+    echo "$cidade", " $dia", " de $mes", " de $ano";
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+    $teste = "Brasil";
+    $php = "---$teste---\n";
+    echo "$php";
+?>
