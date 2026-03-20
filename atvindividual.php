71d26cb98924c673e9459be0d85e6965345f241a
Bruno <bruno.l.motta@unesp.br>
2026-03-19 21:07:47 -0300

    nova atividade

--- atvindividual.php
+++ atvindividual.php
@@ -1,7 +1,7 @@
 <?php
-$cidade = "Bauru";
-$dia = 1;
-$mes = "março";
-$ano = 2024;
-    echo "$cidade", " $dia", " de $mes", " de $ano";
+    $cidade = "Bauru";
+    $dia = 1;
+    $mes = "março";
+    $ano = 2024;
+    echo "<strong>$cidade</strong>, $dia de $mes de $ano";
 ?>
